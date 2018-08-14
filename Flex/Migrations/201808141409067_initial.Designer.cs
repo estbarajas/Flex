557c9ffb116ff10e3037a19e10f3073c3837e326
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-08-14 09:12:46 -0500

    Added Manager Role

--- Flex/Migrations/201808141409067_initial.Designer.cs
+++ Flex/Migrations/201808141409067_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Flex.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201808141358213_init"; }
+            get { return "201808141409067_initial"; }
         }
         
         string IMigrationMetadata.Source
