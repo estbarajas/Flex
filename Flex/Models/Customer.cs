4fe5041af05f079858a5fac9c6c229f7cedbeae7
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-08-20 08:59:16 -0500

    Added customer model

---
+++ Flex/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Flex.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Membership Id")]
+        public string MembershipId { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
