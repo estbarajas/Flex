3e9e246faa7c96fb0775bc774a469116280536f9
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-08-20 10:36:19 -0500

    Fixed foreign key issue with user logged in

--- Flex/Models/CustomerClasses.cs
+++ Flex/Models/CustomerClasses.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,26 @@ namespace Flex.Models
 {
     public class CustomerClasses
     {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Class")]
+        public int? ClassId { get; set; }
+        public Classes Class { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public IEnumerable<Classes> Classes { get; set; }
     }
 }
