29cb528a7e8b9fab3dd55f76ca700e64364b195b
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-08-20 09:18:39 -0500

    Classes model created

---
+++ Flex/Models/Classes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Flex.Models
+{
+    public class Classes
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Class Name")]
+        public string Name { get; set; }
+        [Display(Name = "Class Time")]
+        public string Time { get; set; }
+    }
+}
