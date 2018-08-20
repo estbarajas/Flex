6ad130c531e00af9a53ab87160c34ce0295aa703
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-08-20 08:37:48 -0500

    Fixed manager class model
    created in the wrong place

---
+++ Flex/Models/Manager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Flex.Models
+{
+    public class Manager
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
